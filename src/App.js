b8260f10526a66ef03312e212f34402465044cf6
Shubham Chaudhary <kshubham9525@gmail.com>
2023-10-04 19:05:59 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './Home';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      <h2> Deployed First Project</h2>
-      </header>
+      <Home />
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import logo from './logo.svg';
+
+function Home() {
+  return (
+    <div>
+
+        <img src={logo} className="App-logo" alt="logo" />
+
+    </div>
+  )
+}
+
+export default Home
